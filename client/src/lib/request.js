93dec115bbf850decda77434865dcefed6713007
HubanHubanov <huban.hubanov@gmail.com>
2024-07-18 20:11:55 +0300

    Modify requester

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,13 +1,28 @@
-export const request = async (method, url, data) => {
+const buildOptions = (data) => {
+    const options = {};
+
+    if(data) {
+        options.headers = {
+            "content-type": "application/json"
+        }
+        options.body = JSON.stringify(data)
+    }
+
+    return options
+}
+
+ const request = async (method, url, data) => {
     const response = await fetch(url, {
+        ...buildOptions(data),
         method
     });
-
-    // if(!response.ok) {
-    //     throw new Error("There is un error");
-    // }
 
     const result = await response.json();
 
     return result;
 }
+
+export const get =  request.bind(null, "GET");  
+export const post =  request.bind(null, "POST");  
+export const put =  request.bind(null, "PUT");  
+export const remove =  request.bind(null, "DELETE");  
