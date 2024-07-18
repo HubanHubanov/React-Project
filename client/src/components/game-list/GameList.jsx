034800a937a0b9edf2ae17c9c078d6e64ab04810
HubanHubanov <huban.hubanov@gmail.com>
2024-07-18 18:32:41 +0300

    Add get all games service method Add requester

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+const baseUrl = "http://localhost:3030/jsonstore";
+
+import * as gameService from "../../services/gameService";
+
+
 export default function GameList() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => setGames(result));
+            
+    }, []);
+
+    console.log(games);
+
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
@@ -11,24 +27,8 @@ export default function GameList() {
                 </div>
 
             </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg" />
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-
+           
+          
             <h3 className="no-articles">No articles yet</h3>
         </section>
 
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,7 +1,14 @@
-const baseUrl = "http://localhost:3030/jsonstore"
+const baseUrl = "http://localhost:3030/jsonstore/games";
+import { request } from "../lib/request";
+
+export const getAll = async () => {
+   const result = await request("GET", baseUrl);
+
+   return Object.values(result);
+}   
 
 export const create = async (gameData) => {
-   const response = await fetch(`${baseUrl}/games`, {
+   const response = await fetch(`${baseUrl}`, {
         method: "POST",
         headers: {
             "content-type": "application/json"
@@ -12,3 +19,4 @@ export const create = async (gameData) => {
 
     return result
 }
+
