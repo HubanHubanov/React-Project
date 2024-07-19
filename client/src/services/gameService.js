930a2d132299905d0611bad6f40d344bbc6bfc5e
HubanHubanov <huban.hubanov@gmail.com>
2024-07-19 13:17:23 +0300

    Add new comment

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,5 +1,5 @@
-const baseUrl = "http://localhost:3030/jsonstore/games";
 import * as request from "../lib/request";
+const baseUrl = "http://localhost:3030/jsonstore/games";
 
 export const getAll = async () => {
    const result = await request.get(baseUrl);
